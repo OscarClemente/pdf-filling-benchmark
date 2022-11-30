f134ddb9c31ce2ec7a068684c534889bd325be89
OscarClemente <oscar.clemente@gmail.com>
2022-11-30 18:57:50 +0100

    refactor: Small cleanup

--- getPDFs.sh
+++ getPDFs.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# to check that the pdf generation is valid
 docker cp pdfeditor-orchestrator-1:/app/node.pdf .
 docker cp pdfeditor-orchestrator-1:/app/go.pdf .
 #docker cp pdfeditor-orchestrator-1:/app/golib.pdf .
--- orchestrator/main.go
+++ orchestrator/main.go
@@ -15,27 +15,8 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test Go web app.")
 }
 
-func askNodePdfFill(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received ask request")
-
-	resp, err := http.Get("http://nodepdffill:3001")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Fprintf(w, "Test Go web app asks Node web app: %s", body)
-}
-
 func setupRoutes() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/nodePdfFill", askNodePdfFill)
 }
 
 func main() {
